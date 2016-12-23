a9edef2568c04bcdfc91b4164144c568046b84c2
Daniel Dawson <danieldawson2011@hotmail.com>
2016-12-23 16:37:13 +0000

    Added .htaccess files

--- app/core/App.php
+++ app/core/App.php
@@ -6,11 +6,13 @@
         protected $params =[];
         
         public function __construct() {
-            
+            $this->parseUrl();
         }
         
         public function parseUrl() {
-            
+            if(isset($_GET['url'])) {
+                echo $_GET['url'];
+            }
         }
     }
 ?>
